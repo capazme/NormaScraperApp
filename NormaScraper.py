9d7f89bb255727be972ab943b33db1d50b2ffae1
capazme <guglielmo.puzio00@gmail.com>
2024-04-08 18:49:36 +0200

    restyle e tooltip

--- NormaScraper.py
+++ NormaScraper.py
@@ -1,128 +1,137 @@
 import tkinter as tk
+from tkinter import ttk, messagebox, scrolledtext, filedialog
 import webbrowser
-from tkinter import messagebox, scrolledtext, Scrollbar, filedialog
-from urn import get_urn_and_extract_data
 import pyperclip
-
-def copia_output():
-    try:
-        testo_da_copiare = output_text.get("1.0", tk.END)  # Estrae il testo dall'area di output
-        pyperclip.copy(testo_da_copiare)  # Copia il testo nel clipboard del sistema
-        messagebox.showinfo("Copia", "Testo copiato negli appunti!")  # Opzionale: mostra un messaggio di conferma
-    except Exception as e:
-        messagebox.showerror("Errore", "Impossibile copiare il testo: {}".format(e))
-
-def apri_url(url):
-    webbrowser.open(url, new=2)
+from urn import get_urn_and_extract_data
+class Tooltip:
+    def __init__(self, widget, text):
+        self.widget = widget
+        self.text = text
+        self.tooltip_window = None
+        self.widget.bind("<Enter>", self.show_tooltip)
+        self.widget.bind("<Leave>", self.hide_tooltip)
     
-def crea_link(ui, text, url, row, column):
-    link = tk.Label(ui, text=text, fg="blue", cursor="hand2")
-    link.bind("<Button-1>", lambda e: apri_url(url))
-    link.grid(row=row, column=column)  # Usa grid con parametri row e column
-    return link
-
-def clear_all_fields():
-    # Imposta il contenuto di ogni Entry a una stringa vuota o a un valore predefinito
-    act_type_entry.delete(0, tk.END)
-    date_entry.delete(0, tk.END)
-    act_number_entry.delete(0, tk.END)
-    article_entry.delete(0, tk.END)
-    extension_entry.delete(0, tk.END)
-    version_date_entry.delete(0, tk.END)
-    comma_entry.delete(0, tk.END)
-    # Assicurati di aggiungere qui il codice per resettare eventuali altri campi che potresti avere.
-
-def save_as_xml():
-    save_path = filedialog.asksaveasfilename(initialdir="/Users/guglielmo/Desktop/CODE/Normattiva-scraper/NormaScraperApp/atti_scaricati", defaultextension=".xml", filetypes=[("XML files", "*.xml")])
-    if save_path:
-        # Chiamata alla funzione modificata con il nuovo parametro del percorso di salvataggio
-        fetch_act_data(save_xml_path=save_path)
-
-def fetch_act_data(save_xml_path=None):
-    act_type = act_type_entry.get()
-    date = date_entry.get()
-    act_number = act_number_entry.get()
-    article = article_entry.get()
-    extension = extension_entry.get()
-    version = version_var.get()
-    version_date = version_date_entry.get()
-    comma = comma_entry.get()
+    def show_tooltip(self, event=None):
+        if self.tooltip_window:
+            return
+        x, y, _, _ = self.widget.bbox("insert")
+        x += self.widget.winfo_rootx() + 20
+        y += self.widget.winfo_rooty() + 20
+        self.tooltip_window = tk.Toplevel(self.widget)
+        self.tooltip_window.wm_overrideredirect(True)
+        self.tooltip_window.wm_geometry("+%d+%d" % (x, y))
+        label = tk.Label(self.tooltip_window, text=self.text, background="lightyellow", borderwidth=1, relief="solid")
+        label.pack(ipadx=1, ipady=1)
     
-    try:
-        data = get_urn_and_extract_data(act_type, date, act_number, article, extension, comma, version, version_date, save_xml_path=save_xml_path)
-        if data:
-            # Aggiorna il widget di testo con i dati recuperati
-            output_text.delete('1.0', tk.END)  # Pulisce il widget di testo prima di inserire nuovi dati
-            output_text.insert(tk.END, str(data[0]))  # Inserisce i dati nel widget di testo
-            crea_link(root, "Apri URN Normattiva", data[1], 9, 1)
-            pass
-    except Exception as e:
-        messagebox.showerror("Errore", f"Si è verificato un errore: {e}")
-    
+    def hide_tooltip(self, event=None):
+        if self.tooltip_window:
+            self.tooltip_window.destroy()
+            self.tooltip_window = None
+            
+class NormaScraperApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("NormaScraper - Beta")
+        self.setup_style()
+        self.create_widgets()
 
-# Inizializza la finestra principale
-root = tk.Tk()
-root.title("NormaScraper v2")
+    def setup_style(self):
+        style = ttk.Style()
+        style.theme_use('clam')
+        style.configure('TButton', font=('Helvetica', 10))
+        style.configure('TEntry', padding=5)
+        style.configure('TLabel', font=('Helvetica', 10))
+        style.configure('TRadiobutton', font=('Helvetica', 10))
+        style.configure('Tooltip.TLabel', background='lightyellow', font=('Helvetica', 8))
 
-# Crea e organizza gli elementi dell'interfaccia
-tk.Label(root, text="Tipo atto:").grid(row=0, column=0)
-act_type_entry = tk.Entry(root)
-act_type_entry.grid(row=0, column=1)
-act_type_entry.insert(0, "legge")  # Valore di default per il tipo di atto
+    def create_widgets(self):
+        self.mainframe = ttk.Frame(self.root, padding="5")
+        self.mainframe.grid(column=0, row=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+        self.mainframe.columnconfigure(0, weight=1)
+        self.mainframe.rowconfigure(0, weight=1)
 
-tk.Label(root, text="Data (YYYY-MM-DD, esteso o solo anno):").grid(row=1, column=0)
-date_entry = tk.Entry(root)
-date_entry.grid(row=1, column=1)
-date_entry.insert(0, "7 agosto 1990")  # Valore di default per la data
+        # Input fields
+        self.act_type_entry = self.create_labeled_entry("Tipo atto:", "Inserisci il tipo di atto", 0, 0)
+        self.date_entry = self.create_labeled_entry("Data (YYYY-MM-DD, esteso o solo anno):", "Inserisci il tipo di atto", 1, 0)
+        self.act_number_entry = self.create_labeled_entry("Numero atto:", "Inserisci il tipo di atto", 2, 0)
+        self.article_entry = self.create_labeled_entry("Articolo:", "Inserisci il tipo di atto", 3, 0)
+        self.extension_entry = self.create_labeled_entry("Estensione:", "Inserisci il tipo di atto", 4, 0)
+        self.comma_entry = self.create_labeled_entry("Comma:", "Inserisci il tipo di atto", 5, 0)
 
-tk.Label(root, text="Numero atto:").grid(row=2, column=0)
-act_number_entry = tk.Entry(root)
-act_number_entry.grid(row=2, column=1)
-act_number_entry.insert(0, "241")  # Valore di default per il numero di atto
+        # Version radio buttons
+        tk.Label(self.mainframe, text="Versione:").grid(row=6, column=0)
+        self.version_var = tk.StringVar(value="vigente")
+        tk.Radiobutton(self.mainframe, text="Originale", variable=self.version_var, value="originale").grid(row=6, column=1)
+        tk.Radiobutton(self.mainframe, text="Vigente", variable=self.version_var, value="vigente").grid(row=6, column=2)
 
-tk.Label(root, text="Articolo:").grid(row=3, column=0)
-article_entry = tk.Entry(root)
-article_entry.grid(row=3, column=1)
-#article_entry.insert(0, "2")  # Valore di default per l'articolo
+        self.version_date_entry = self.create_labeled_entry("Data versione atto (se applicabile):", "Inserisci la data di versione dell'atto se disponibile", 7, 0)
 
-tk.Label(root, text="Estensione articolo:").grid(row=4, column=0)
-extension_entry = tk.Entry(root)
-extension_entry.grid(row=4, column=1)
-#extension_entry.insert(0, "bis")  # Valore di default per l'estensione articolo
+        # Buttons
+        fetch_button = tk.Button(self.mainframe, text="Estrai dati", command=self.fetch_act_data)
+        fetch_button.grid(row=8, column=0, columnspan=3)
+        save_xml_button = tk.Button(self.mainframe, text="Salva come XML", command=self.save_as_xml)
+        save_xml_button.grid(row=8, column=1, columnspan=3)
+        clear_button = tk.Button(self.mainframe, text="Cancella", command=lambda: self.clear_all_fields([self.act_type_entry, self.date_entry, self.act_number_entry, self.article_entry, self.extension_entry, self.comma_entry, self.version_date_entry]))
+        clear_button.grid(row=8, column=2)
+        copia_button = tk.Button(self.mainframe, text="Copia Testo", command=self.copia_output)
+        copia_button.grid(row=9, column=0)
 
-tk.Label(root, text="Comma:").grid(row=5, column=0)
-comma_entry = tk.Entry(root)
-comma_entry.grid(row=5, column=1)
+        self.output_text = scrolledtext.ScrolledText(self.mainframe, wrap=tk.WORD, width=130, height=30)
+        self.output_text.grid(row=10, column=0, columnspan=3, pady=10)
 
-tk.Label(root, text="Versione:").grid(row=6, column=0)
-version_var = tk.StringVar(value="vigente")  # 'originale' è già impostato come valore di default
-tk.Radiobutton(root, text="Originale", variable=version_var, value="originale").grid(row=6, column=1)
-tk.Radiobutton(root, text="Vigente", variable=version_var, value="vigente").grid(row=6, column=2)
+    def create_labeled_entry(self, label_text, tooltip_text, row, col):
+        ttk.Label(self.mainframe, text=label_text).grid(row=row, column=col, sticky=tk.W)
+        entry = ttk.Entry(self.mainframe)
+        entry.grid(row=row, column=col+1, sticky=(tk.W, tk.E), padx=2, pady=2)
+        Tooltip(entry, tooltip_text)
+        return entry
 
-tk.Label(root, text="Data versione vigente:").grid(row=7, column=0)
-version_date_entry = tk.Entry(root)
-version_date_entry.grid(row=7, column=1)
-#version_date_entry.insert(0, "2023-01-01")  # Valore di default per la data della versione vigente
+    def copia_output(self):
+        content = self.output_text.get("1.0", tk.END)
+        pyperclip.copy(content)
+        messagebox.showinfo("Copia", "Testo copiato negli appunti!")
 
-# Bottoni
-fetch_button = tk.Button(root, text="Estrai dati", command=fetch_act_data)
-fetch_button.grid(row=8, column=0, columnspan=3)
+    def apri_url(self, url):
+        webbrowser.open_new_tab(url)
 
-save_xml_button = tk.Button(root, text="Salva come XML", command=save_as_xml)
-save_xml_button.grid(row=8, column=1, columnspan=3)
+    def clear_all_fields(self, entries):
+        for entry in entries:
+            entry.delete(0, tk.END)
 
-clear_button = tk.Button(root, text="Cancella", command=clear_all_fields)
-clear_button.grid(row=8, column=2)  # Modifica il valore di row e column secondo le tue necessità
+    def save_as_xml(self):
+        file_path = filedialog.asksaveasfilename(
+            title="Salva come XML",
+            initialdir="/",
+            filetypes=[("XML files", "*.xml")],
+            defaultextension=".xml"
+        )
+        if file_path:
+            self.fetch_act_data(save_xml_path=file_path)
 
-copia_button = tk.Button(root, text="Copia Testo", command=copia_output)
-copia_button.grid(row=9, column=0)  # Aggiusta i valori di 'row' e 'column' a seconda del layout della tua UI
+    def fetch_act_data(self, save_xml_path=None):
+        act_type = self.act_type_entry.get()
+        date = self.date_entry.get()
+        act_number = self.act_number_entry.get()
+        article = self.article_entry.get()
+        extension = self.extension_entry.get()
+        version = self.version_var.get()
+        version_date = self.version_date_entry.get()
+        comma = self.comma_entry.get()
+        
+        try:
+            data, url = get_urn_and_extract_data(act_type, date, act_number, article, extension, comma, version, version_date, save_xml_path=save_xml_path)
+            self.output_text.delete('1.0', tk.END)
+            self.output_text.insert(tk.END, data)
+            self.crea_link("Apri URN Normattiva", url, 9, 1)
+        except Exception as e:
+            messagebox.showerror("Errore", str(e))
 
-# Widget di testo per l'output
-output_text = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=130, height=30)
-output_text.grid(row=10, column=0, columnspan=3, pady=10)
+    def crea_link(self, text, url, row, column):
+        link = tk.Label(self.mainframe, text=text, fg="blue", cursor="hand2")
+        link.bind("<Button-1>", lambda e: self.apri_url(url))
+        link.grid(row=row, column=column)
 
-
-
-
-
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = NormaScraperApp(root)
+    root.mainloop()
